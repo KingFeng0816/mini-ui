56534d512d340bda8a04bac24e3f4bf21d1b47c1
王金峰 <>
2020-01-13 09:04:22 +0800

    商户+商品详情

--- src/store/actions/purchaser.js
+++ src/store/actions/purchaser.js
@@ -2,7 +2,7 @@
 import { RequestService } from './request.js'
 
 // 获取轮播图数据
-export const GetSwiperData = function (myData) {
+export const GetSwiperData = function (callBack) {
   wx.request({
     url: RequestService.ServiceURL+'/mtp-wx/getHomeBannerData', //仅为示例，并非真实的接口地址
     data: {
@@ -11,25 +11,43 @@ export const GetSwiperData = function (myData) {
       'content-type': 'application/json' // 默认值
     },
     success: function(res) {
-      typeof myData == "function" && myData(res)
+      typeof callBack == "function" && callBack(res)
     }
   })
-}
+};
 
 // 获取店铺列表数据
-export const GetStoreListData = function (myData) {
-  wx.request({
-    url: RequestService.ServiceURL+'/mtp-wx/getStoreListData', //仅为示例，并非真实的接口地址
-    data: {
-    },
-    header: {
-      'content-type': 'application/json' // 默认值
-    },
-    success: function(res) {
-      typeof myData == "function" && myData(res)
-    }
-  })
-}
+export const GetStoreListData = function (callBack) {
+    wx.request({
+        url: RequestService.ServiceURL+'/mtp-wx/getStoreListData', //仅为示例，并非真实的接口地址
+        data: {
+        },
+        header: {
+            'content-type': 'application/json' // 默认值
+        },
+        success: function(res) {
+            typeof callBack == "function" && callBack(res)
+        }
+    })
+};
+
+// 获取店铺列表数据
+export const GetStoreProductListData = function (parameter,callBack) {
+    wx.request({
+        url: RequestService.ServiceURL+'/mtp-wx/getStoreProductListData', //仅为示例，并非真实的接口地址
+        data: {
+          "storeId":parameter
+        },
+        method:'POST',
+        header: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json' // 默认值
+        },
+        success: function(res) {
+            typeof callBack == "function" && callBack(res)
+        }
+    })
+};
 
 
 
